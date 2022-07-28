6e71e887dd119af11d8fccc98fb136d0c0b617c2
maikelordaz <maikelordaz@gmail.com>
2022-07-28 15:59:09 -0400

    Initial Setup

---
+++ components/ManualHeader.jsx
@@ -0,0 +1,10 @@
+/*
+* Los archivos ***.jsx es lo mismo que ***.js pero como elemento de React, asi puedo modularizar 
+* mi proyecto, y reciclar codigo a usar en varias paginas.
+* Para usarlo debo exportarlo aqui, e importarlo donde lo voy a usar
+* Este script es para la forma dificil
+*/
+
+export default function ManualHeader() {
+    return(<div>Hi from Header!</div>)
+}
